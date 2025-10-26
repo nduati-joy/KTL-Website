773fef81469907fbab0dd2afc4047db708267032
nduati-joy <wanjirujoy002@gmail.com>
2025-10-26 22:46:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 Js
 
-function openMenu(){
-  document.getElementById("side.menu").style.width="75%";
-}
 
-function closedMenu(){
-  document.getElementById("side.menu").style.width="0";
-}
-  document.addEventlistener('click', function(event){
-    const menu = document.getElementById("side-menu");
-     const icon = document.querySelector("menu-icon");
-    if(!menu.contains(event.target) && !
-       icon.contains(event. target)) {
-      closeMenu();
-    }
-    ));
+  document.addEventlistener('DOMContentLoaded', function(){
+    const toggle = document.getElementById("menu-toggle");
+     const menu = document.getElementById("side-menu");
+    toggle.addEventlistener("click", function(){
+      menu.classlist.toggle("active");
+    });
+  });
   
 
     
