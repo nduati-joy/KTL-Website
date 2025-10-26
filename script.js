13d1b2c52bd31e93716597e5365393b02a7afbb8
nduati-joy <wanjirujoy002@gmail.com>
2025-10-26 22:58:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 Js
 
 
-  document.addEventlistener('DOMContentLoaded', function(){
-    const toggle = document.getElementById("menu-toggle");
-     const menu = document.getElementById("side-menu");
+  document.addEventListener("DOMContentLoaded", function(){
+    const toggle = 
+      document.getElementById("menu-toggle");
+     const menu = 
+       document.getElementById("side-menu");
     toggle.addEventlistener("click", function(){
-      menu.classlist.toggle("active");
+      menu.classList.toggle("active");
     });
   });
   
