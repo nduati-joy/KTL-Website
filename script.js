262e3e70830c9e64af24ef3846e748b39957409b
nduati-joy <wanjirujoy002@gmail.com>
2025-10-26 18:16:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ function toggleMenu() {
     const menu = document.getElementById("side-menu");
     const overlay = document.getElementById("ovet lay");
 
-    menu.classlist.toggle("open");
-    overlay.classlist.toggle("show");
-}
+    MenuBtn.addEventListener("click",()=> {
+        menu.classlist.toggle("open");
+        overlay.classlist.toggle("show");
+    });
+
+    overlay..addEventListener("click", ()=> {
+        menu.classlist.remove("open");
+        overlay.classlist.remove("show");
+    });
+
+
+    
+
     
     
 
